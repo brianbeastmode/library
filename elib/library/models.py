941799691cb1b02b17f5714e2c4744f2422d3a26
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2020-12-12 14:04:55 +0800

    edited library page
    css and html

--- elib/library/models.py
+++ elib/library/models.py
@@ -4,6 +4,7 @@ class Books(models.Model):
     bookName = models.CharField(max_length=100)
     bookAuthor = models.CharField(max_length=100)
     bookDescription = models.TextField()
+    bookCategory = models.CharField(max_length=100, blank=True)
     yearPublished = models.IntegerField()
     bookImage = models.ImageField(upload_to='covers', blank=True)
     isbn = models.IntegerField(blank=True)

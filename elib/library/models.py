eefff21c4985f3bd15460db21c87b71a339bc41a
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2020-12-16 13:07:31 +0800

    added and edited
    added bookfile and edited bookname

--- elib/library/models.py
+++ elib/library/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Books(models.Model):
-    bookName = models.CharField(max_length=100)
+    bookTitle = models.CharField(max_length=100)
     bookAuthor = models.CharField(max_length=100)
     bookDescription = models.TextField()
     bookCategory = models.CharField(max_length=100, blank=True)
     yearPublished = models.IntegerField()
+    bookFile = models.FileField(upload_to='books', blank=True)
     bookImage = models.ImageField(upload_to='covers', blank=True)
     isbn = models.IntegerField(blank=True)
     pages = models.IntegerField()

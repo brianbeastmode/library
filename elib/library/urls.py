805045ec92b6048588a086f3b257381670ad5f32
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2020-12-16 13:09:09 +0800

    Update urls.py
    added url for every book

--- elib/library/urls.py
+++ elib/library/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('library', views.library, name = 'library'),
     path('staff', views.staff, name = 'staff'),
     path('services', views.services, name = 'services'),
-    path('about', views.about, name = 'about')
+    path('about', views.about, name = 'about'),
+    path('library/<int:id>', views.book, name = 'libpreview'),
+    
 ]

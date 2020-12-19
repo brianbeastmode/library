4750ef339b366f9f30a817d9220859f9808b56ac
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2020-12-19 15:47:38 +0800

    Update settings.py

--- elib/elib/settings.py
+++ elib/elib/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
     'django.contrib.sites',
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

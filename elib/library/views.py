2cedced718ffa701b0d4f9b784a825385333f0ea
brianbeastmode <75476483+brianbeastmode@users.noreply.github.com>
2020-12-19 15:48:33 +0800

    Update views.py
    Changed not to is and added book view

--- elib/library/views.py
+++ elib/library/views.py
@@ -5,18 +5,18 @@ from .models import Books
 
 def index(request):
 
-    if not request.user.is_authenticated:
-        return render(request, "login.html")
-    else:
+    if request.user.is_authenticated:
         return render(request, "index.html")
+    else:
+        return render(request, "login.html")
 
 def library(request):
     book = Books.objects.all()
 
-    if not request.user.is_authenticated:
-        return render(request, "login.html")
-    else:
+    if request.user.is_authenticated:
         return render(request, "library.html", {'books' : book})
+    else:
+        return render(request, "login.html")
 
 def book(request, id):
     book = Books.objects.get(id=id)
@@ -24,19 +24,19 @@ def book(request, id):
 
 
 def staff(request):
-    if not request.user.is_authenticated:
-        return render(request, "login.html")
-    else:
+    if request.user.is_authenticated:
         return render(request, "staff.html")
+    else:
+        return render(request, "login.html")
 
 def services(request):
-    if not request.user.is_authenticated:
-        return render(request, "login.html")
-    else:
+    if request.user.is_authenticated:
         return render(request, "services.html")
+    else:
+        return render(request, "login.html")
 
 def about(request):
-    if not request.user.is_authenticated:
-        return render(request, "login.html")
-    else:
+    if request.user.is_authenticated:
         return render(request, "about.html")
+    else:
+        return render(request, "login.html")
